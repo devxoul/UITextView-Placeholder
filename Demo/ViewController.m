55f9962b2e80687c8641ae32526547cd3af3c95a
Suyeol Jeon <devxoul@gmail.com>
2019-07-31 16:11:30 +0900

    Update demo to use placeholder attribute

--- Demo/ViewController.m
+++ Demo/ViewController.m
@@ -31,11 +31,11 @@
     UITextView *textView = [[UITextView alloc] init];
     textView.frame = CGRectMake(50, 120, 200, 200);
     textView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-//    textView.placeholder = @"Are you sure you don\'t want to reconsider? Could you tell us why you wish to leave StyleShare? Your opinion helps us improve StyleShare into a better place for fashionistas from all around the world. We are always listening to our users. Help us improve!";
-    NSDictionary *attrs = @ {
-        NSFontAttributeName: [UIFont boldSystemFontOfSize:20],
-    };
-    textView.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Hi" attributes:attrs];
+    textView.placeholder = @"Are you sure you don\'t want to reconsider? Could you tell us why you wish to leave StyleShare? Your opinion helps us improve StyleShare into a better place for fashionistas from all around the world. We are always listening to our users. Help us improve!";
+//    NSDictionary *attrs = @ {
+//        NSFontAttributeName: [UIFont boldSystemFontOfSize:20],
+//    };
+//    textView.attributedPlaceholder = [[NSAttributedString alloc] initWithString:@"Hi" attributes:attrs];
     textView.font = [UIFont systemFontOfSize:15];
     textView.layer.borderColor = UIColor.redColor.CGColor;
     textView.layer.borderWidth = 1.0;
